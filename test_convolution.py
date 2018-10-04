5fd933d9601732cd73e70bc193cac22cc05ef57a
Victor Joos <victorjoosdtb@gmail.com>
2018-10-04 22:31:25 +0200

    changed channels keras...

--- test_convolution.py
+++ test_convolution.py
@@ -7,9 +7,9 @@ from keras import backend as K
 w = [np.arange(3*3*5*2).reshape(3, 3, 5, 2)]
 
 model = Sequential()
-model.add(Conv2D(2, kernel_size=3, strides=1, input_shape=(6,6,5),
-padding='same', use_bias=False))
+model.add(Conv2D(2, kernel_size=3, strides=1, input_shape=(5,6,6),
+padding='same', use_bias=False, data_format='channels_first'))
 model.set_weights(w)
-score = model.predict([np.arange(1*6*6*5).reshape(1,6,6,5)])
+score = model.predict([np.arange(1*6*6*5).reshape(1,5,6,6)])
 
 print(score)
